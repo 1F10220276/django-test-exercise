3259e003ec2f849cb0eb00bb01612f1af25ae1d6
1F10220276 <s1F102202763@iniag.org>
2024-07-04 12:08:39 +0900

    修正２

--- todo/tests.py
+++ todo/tests.py
@@ -87,11 +87,11 @@ class TodoViewTestCase(TestCase):
         task = Task(title='task1', due_at=timezone.make_aware(datetime(2024, 7, 1)))
         task.save()
         client = Client()
-        response = client.get('/{}/'. format(task.pk))
+        response = client.get('/{}/'.format(task.pk))
 
-        self. assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 200)
         self.assertEqual(response.templates[0].name, 'todo/detail.html')
-        self. assertEqual(response.context['task'], task)
+        self.assertEqual(response.context['task'], task)
 
     def test_detail_get_fail(self):
         client = Client()
